d35fbab8dbccbbdecd0220566bb84ec1dabbe044
raytsson <raytssonmoraes@hotmail.com>
2024-07-24 10:14:14 -0300

    Iniciado o Resource de chamados

---
+++ src/main/java/raytsson/fullstackbackend/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package raytsson.fullstackbackend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import raytsson.fullstackbackend.domain.Chamado;
+import raytsson.fullstackbackend.repositories.ChamadoRepository;
+import raytsson.fullstackbackend.services.exceptions.ObjectNotFoundException;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> obj = chamadoRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! " + id));
+    }
+}
