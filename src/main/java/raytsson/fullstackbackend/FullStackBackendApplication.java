02486e4212ca44fff0d17e5c018d8286e4a00313
raytsson <raytssonmoraes@hotmail.com>
2024-08-21 14:24:23 -0300

    Corrigido erro de conexão

--- src/main/java/raytsson/fullstackbackend/FullStackBackendApplication.java
+++ src/main/java/raytsson/fullstackbackend/FullStackBackendApplication.java
@@ -11,6 +11,12 @@ public class FullStackBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FullStackBackendApplication.class, args);
+
+		System.out.println("MYSQLHOST: " + System.getenv("MYSQLHOST"));
+		System.out.println("MYSQLPORT: " + System.getenv("MYSQLPORT"));
+		System.out.println("MYSQLDATABASE: " + System.getenv("MYSQLDATABASE"));
+		System.out.println("MYSQLUSER: " + System.getenv("MYSQLUSER"));
+		System.out.println("MYSQLPASSWORD: " + System.getenv("MYSQLPASSWORD"));
 	}
 
 }
